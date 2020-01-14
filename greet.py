fe3b532277562480e78dda432dcf16edd3e472c9
tektips <tektipsenquires@gmail.com>
2020-01-14 10:31:53 -0500

    Create greet.py

---
+++ greet.py
@@ -0,0 +1,14 @@
+# greet.py
+
+def greet(name):
+ 
+	print("Hello, " + name + ". Good morning dude!")
+	
+
+greet("Mat")
+
+Hello, Mat. Good morning dude!
+
+## If we intend the greet method, we get the below errors
+
+** IndentationError: unindent does not match any outer indentation level **
